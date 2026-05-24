c1524b83f08f9fdc366bf5d99e24a3389faf97cf
Emilia Olszewska <120374833+emiolsz@users.noreply.github.com>
2026-05-24 22:04:17 +0200

    Add files via upload

---
+++ ziola.py
@@ -0,0 +1,9 @@
+# Plik o nazwie: ziola.py
+
+BAZA_ZIOL = {
+    "Rumianek pospolity": {
+        "lacina": "Matricaria chamomilla",
+        "pozyskiwanie": "Koszyczki rumianku zbiera się w słoneczne dni od maja do sierpnia.",
+        "przepis": "☕ **Napar:** 1 łyżkę suszu zalej szklanką wrzątku. Parz 10 minut."
+    }
+}
