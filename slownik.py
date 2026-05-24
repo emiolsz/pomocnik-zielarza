69f64a85181d6acd63fc4d42b6b97a32573520d9
Emilia Olszewska <120374833+emiolsz@users.noreply.github.com>
2026-05-24 23:36:48 +0200

    Add files via upload

---
+++ slownik.py
@@ -0,0 +1,13 @@
+# Plik o nazwie: slownik.py
+
+MAPOWANIE_AI = {
+    "chamomile": "Rumianek pospolity",
+    "daisy": "Rumianek pospolity",
+    "birch": "Brzoza brodawkowata",
+    "bark": "Brzoza brodawkowata",
+    "agaric": "Boczniak ostrygowaty",
+    "mushroom": "Boczniak ostrygowaty",
+    "bolete": "Borowik szlachetny",
+    "earthstar": "Błyskoporek podkorowy",
+    "hen-of-the-woods": "Boczniak ostrygowaty"
+}
