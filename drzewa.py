03fe7fb3f098c8afb3dfa390419ea1656303323a
Emilia Olszewska <120374833+emiolsz@users.noreply.github.com>
2026-05-24 22:26:11 +0200

    Update drzewa.py

---
+++ drzewa.py
@@ -0,0 +1,7 @@
+BAZA_DRZEW = {
+    "Brzoza brodawkowata": {
+        "lacina": "Betula pendula",
+        "pozyskiwanie": "Korę pozyskuje się wiosną ze ściętych gałęzi lub pni.",
+        "przepis": "🧪 **Odwis z kory:** 1 łyżkę rozdrobnionej kory zalej szklanką wody, gotuj 5 minut."
+    }
+}
